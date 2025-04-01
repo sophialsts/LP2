a5709f953da6c1a5a3c94516fd2cb57327e2c804
Sophia <sophialimasantos4@gmail.com>
2025-04-01 14:01:18 -0300

    questao do Detran ajustada

--- blueJ/CarteiraDetran.java
+++ blueJ/CarteiraDetran.java
@@ -2,12 +2,22 @@ package blueJ;
 
 public class CarteiraDetran
 {  
-    int pontos;
+    int pontos, numeroCarteira;
+    String tipoCarteira;
+    long CPF;
     boolean validade;
+    int mesAtual, anoAtual, mesExpedicao, anoExpedicao;
     
-    CarteiraDetran () {
+    CarteiraDetran (int num, long cpf, String tipo, int mesA, int anoA, int mesE, int anoE) {
+        this.tipoCarteira = tipo;
         this.pontos = 0;
+        this.CPF = cpf;
+        this.numeroCarteira = num;
         this.validade = true;
+        this.mesAtual = mesA;
+        this.anoAtual = anoA;
+        this.mesExpedicao = mesE;
+        this.anoExpedicao = anoE;
     }
     
     void aumentarPontos (int p) {
@@ -27,8 +37,20 @@ public class CarteiraDetran
     }
     
     void verificaValidade () {
-        if (this.validade) System.out.println("Sua carteira esta valida.");
-        else System.out.println("Sua carteira nao esta valida.");
+        if(this.anoAtual - this.anoExpedicao >= 10){
+            if(this.anoAtual - this.anoExpedicao == 10) {
+                if(this.mesExpedicao <= this.mesAtual) System.out.println("Sua carteira esta vencida.");
+                else {
+                    if(this.validade) System.out.println("Sua carteira esta valida.");
+                    else System.out.println("Sua carteira nao esta valida.");
+                }
+            }
+            else System.out.println("Sua carteira esta vencida.");
+        }
+        else if(this.validade) {
+            System.out.println("Sua carteira esta valida");
+        }
+        else System.out.println("Sua carteira nao esta valida");
     }
     
 }
